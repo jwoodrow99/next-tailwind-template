7c5ba766e8f9f700283045c7e7af15abe60e9785
Jack Woodrow <jackwoodrow99@gmail.com>
2023-02-26 15:20:11 -0500

    Config documentation and example

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+	content: ['./src/**/*.{js,ts,jsx,tsx}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				poppins: ['Poppins', 'sans-serif'],
+				lato: ['Lato', 'sans-serif'],
+			},
+		},
+	},
+	plugins: [],
+};
